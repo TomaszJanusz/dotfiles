2131de1a1c5c31d7241ee1f7b4b4dd649fb59703
Tomasz Janusz <tomasz.janusz@gmail.com>
2017-08-14 03:19:40 +0200

    Add brew & brew cask scripts

---
+++ brew-cask.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+
+# Install native apps
+brew tap caskroom/versions
+
+# daily
+brew cask install 1password
+brew cask install alfred
+
+# dev
+brew cask install iterm2-beta
+brew cask install imagealpha
+brew cask install imageoptim
+brew cask install tower
+brew cask install visual-studio-code
+brew cask install dash
+
+# Grapic
+brew cask install adobe-photoshop-cc
+brew cask install adobe-illustrator-cc
+brew cask install sketch
+brew cask install sketch-toolbox
+
+# browsers
+brew cask install google-chrome-canary
+brew cask install firefoxnightly
+# brew cask install webkit-nightly
+# brew cask install chromium
+# brew cask install torbrowser
+
+# less often
+brew cask install gpgtools
+
+# pleasure 
+brew cask install spotify
+
+# fonts
+brew tap caskroom/fonts 
+brew cask install font-fira-code
+
+brew tap tomick/homebrew-cask-alternatives
+brew cask install setapp
+
+# Wire
---
+++ brew.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Make sure we’re using the latest Homebrew
+brew update
+
+# Upgrade any already-installed formulae
+brew upgrade
+
+
+# GNU core utilities (those that come with OS X are outdated)
+brew install coreutils
+brew install moreutils
+# GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed
+brew install findutils
+# GNU `sed`, overwriting the built-in `sed`
+brew install gnu-sed --with-default-names
+
+# Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before running `chsh`.
+brew install fish
+sudo echo '/usr/local/bin/fish' >> /etc/shells
+chsh -s /usr/local/bin/fish
+brew install homebrew/completions/brew-cask-completion
+
+# z hopping around folders
+brew install z
+
+# run this script when this file changes guy.
+brew install entr
+
+
+# mtr - ping & traceroute. best.
+brew install mtr
+
+    # allow mtr to run without sudo
+    mtrlocation=$(brew info mtr | grep Cellar | sed -e 's/ (.*//') #  e.g. `/Users/paulirish/.homebrew/Cellar/mtr/0.86`
+    sudo chmod 4755 $mtrlocation/sbin/mtr
+    sudo chown root $mtrlocation/sbin/mtr
+
+
+# Install other useful binaries
+brew install the_silver_searcher
+brew install fzf
+
+brew install git
+brew install imagemagick --with-webp
+brew install node # This installs `npm` too using the recommended installation method
+brew install pv
+brew install rename
+brew install tree
+brew install zopfli
+
+brew install terminal-notifier
+brew install pidcat   # colored logcat guy
+
+brew install ncdu # find where your diskspace went
+
+# Remove outdated versions from the cellar
+brew cleanup
