368728ca3378e6bc3f0b381d43b0087405bfd8ff
Tomasz Janusz <tomasz.janusz@daftcode.pl>
2017-08-15 21:33:52 +0200

    Add .extra.fish loading

--- fish/config.fish
+++ fish/config.fish
@@ -2,3 +2,10 @@ source ~/.config/fish/paths.fish
 source ~/.config/fish/iterm2_shell_integration.fish
 source ~/.config/fish/aliases.fish
 source ~/.config/fish/functions.fish
+
+# for things not checked into git..
+if test -e "$HOME/.extra.fish";
+	source ~/.extra.fish
+end
+
+rvm default
