e7ed28a074fbbb6f85618d8702622bc7fb3d5dfc
Tomasz Janusz <tomasz.janusz@daftcode.pl>
2017-09-10 18:28:32 +0200

    make more flat directory list

--- fish/config.fish
+++ fish/config.fish
@@ -3,6 +3,16 @@ source ~/.config/fish/iterm2_shell_integration.fish
 source ~/.config/fish/aliases.fish
 source ~/.config/fish/functions.fish
 
+set -U fish_path $HOME/dotfiles/fisherman
+
+set fish_function_path $fish_path/functions $fish_function_path
+set fish_complete_path $fish_path/completions $fish_complete_path
+
+for file in $fish_path/conf.d/*.fish
+    source $file
+end
+
+
 # for things not checked into git..
 if test -e "$HOME/.extra.fish";
 	source ~/.extra.fish
