a87787edfe6e28bfbb344c8b79dc87d3c4bb87f6
Tomasz Janusz <tomasz.janusz@gmail.com>
2019-10-18 19:11:31 +0200

    Add Android Dev cfg

--- fish/config.fish
+++ fish/config.fish
@@ -4,4 +4,5 @@ set -g -x fish_greeting ''
 set -g -x DOTFILES_DIR $HOME/dotfiles
 set -g -x EDITOR 'nano'
 set -g -x GOPATH $HOME/Projekty/go
-set -g -x GO15VENDOREXPERIMENT 1
+set -g -x GO15VENDOREXPERIMENT 1set -g fish_user_paths "/usr/local/opt/node@10/bin" $fish_user_paths
+set -g -x ANDROID_HOME $HOME/Library/Android/sdk
